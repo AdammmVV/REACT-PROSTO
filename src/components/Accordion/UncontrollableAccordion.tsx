08a0536b1ec5e84f61b2378f38fc7b5cd3c74333
Adammm <adammm.web@gmail.com>
2023-01-20 00:10:37 +0300

    create uncontrollable components

---
+++ src/components/Accordion/UncontrollableAccordion.tsx
@@ -0,0 +1,36 @@
+import React, {useState} from "react";
+import * as buffer from "buffer";
+
+type AccordionPropsType = {
+    titleValue: string
+}
+
+export const UncontrollableAccordion = (props: AccordionPropsType) => {
+
+    const [collapsed, setCollapsed] = useState(false)
+    return (
+        <div>
+            <UncontrollableAccordionTitle title={props.titleValue} setCollapsed={() => setCollapsed(!collapsed)}/>
+            {!collapsed && <UncontrollableAccordionBody/>}
+        </div>
+    )
+}
+
+type AccordionTitlePropsType = {
+    title: string
+    setCollapsed: ()=> void
+}
+
+const UncontrollableAccordionTitle = (props: AccordionTitlePropsType) => {
+    return <h3 onClick={props.setCollapsed}>-- {props.title} --</h3>
+}
+
+const UncontrollableAccordionBody = () => {
+    return (
+        <ul>
+            <li>1</li>
+            <li>2</li>
+            <li>3</li>
+        </ul>
+    )
+}
