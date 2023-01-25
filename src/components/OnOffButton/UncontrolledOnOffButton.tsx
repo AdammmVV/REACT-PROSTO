52e06ab954c96bbe435b0ea7c7c70b0bbb7ae112
Adammm <adammm.web@gmail.com>
2023-01-25 20:06:32 +0300

    refactor UncontrolledOnOffButton.tsx

--- src/components/OnOffButton/UncontrolledOnOffButton.tsx
+++ src/components/OnOffButton/UncontrolledOnOffButton.tsx
@@ -4,15 +4,24 @@ import {circleStyle, offStyle, onStyle} from "./OnOffButton";
 export const UncontrolledOnOffButton = () => {
     const [onOff, setOnOff] = useState(false)
 
+    const finalOnStyle = {...onStyle, backgroundColor: onOff ? 'green' : 'white'}
+    const finalOffStyle = {...offStyle, backgroundColor: onOff ? 'white' : 'red'}
+    const finalCircleStyle = {...circleStyle, backgroundColor: onOff ? 'green' : 'red'}
+
+
+    const onClicked = () => {
+        setOnOff(true)
+    }
+
     return (
         <div className='onOffButton'>
             <div>
-                <button style={{...onStyle, backgroundColor: onOff ? 'green' : 'white'}} onClick={() => setOnOff(true)}>ON</button>
+                <button style={finalOnStyle} onClick={onClicked}>ON</button>
             </div>
             <div>
-                <button style={{...offStyle, backgroundColor: onOff ? 'white' : 'red'}} onClick={() => setOnOff(false)}>OFF</button>
+                <button style={finalOffStyle} onClick={() => setOnOff(false)}>OFF</button>
             </div>
-            <div style={{...circleStyle, backgroundColor: onOff ? 'green' : 'red'}}>
+            <div style={finalCircleStyle}>
             </div>
         </div>
     )
