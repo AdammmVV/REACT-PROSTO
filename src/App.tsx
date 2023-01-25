37ff6d257eb924e076679fd62b7258980c2e3cc0
Adammm <adammm.web@gmail.com>
2023-01-25 19:12:22 +0300

    add useState for controlled components

--- src/App.tsx
+++ src/App.tsx
@@ -2,24 +2,27 @@ import React, {useState} from 'react';
 import './App.css';
 import Accordion from "./components/Accordion/Accordion";
 import {OnOffButton} from "./components/OnOffButton/OnOffButton";
-import {UncontrollableAccordion} from "./components/Accordion/UncontrollableAccordion";
 import {Rating, RatingStarType} from "./components/Star/Star";
 
 
 const App = () => {
     const [ratingValue, setRatingValue] = useState<RatingStarType>(0)
+    const [accordionCollapsed, setAccordionCollapsed] = useState<boolean>(false)
+    const [onOffButton, setOnOffButton] = useState<boolean>(false)
+
     return (
         <div className="App">
             <PageTitle title={'Hello, samurai! Let\'s go!'}/>
             Article 1
             <Rating value={ratingValue} setRatingValue={setRatingValue}/>
-            <PageTitle title={'Menu'}/>
-            <Accordion titleValue={'Menu'} collapsed={true}/>
-            <Accordion titleValue={'Users'} collapsed={false}/>
-            <UncontrollableAccordion titleValue={'Menu'}/>
-            Article 2
+            {/*<PageTitle title={'Menu'}/>*/}
+            <Accordion titleValue={'Menu'}
+                       collapsed={accordionCollapsed}
+                       setAccordionCollapsed={setAccordionCollapsed} />
+            {/*<UncontrollableAccordion titleValue={'Menu'}/>*/}
+            {/*Article 2*/}
             {/*<UncontrolledRating/>*/}
-            <OnOffButton/>
+            <OnOffButton onOff={onOffButton} setOnOff={setOnOffButton}/>
         </div>
     );
 }
