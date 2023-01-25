9d5a2a290395f64024a12ae20c8417ff4dd73dcb
Adammm <adammm.web@gmail.com>
2023-01-25 19:56:09 +0300

    refactor Star.tsx

--- src/components/Star/Star.tsx
+++ src/components/Star/Star.tsx
@@ -1,22 +1,21 @@
 import React from "react";
 import s from './Star.module.css'
 
-export type RatingStarType = 0 | 1 | 2 | 3 | 4 | 5
+export type RatingStarType = 1 | 2 | 3 | 4 | 5
 
 export type RatingPropsType = {
-    value: RatingStarType
+    value: RatingStarType | 0
     setRatingValue: (value:RatingStarType)=>void
+    star: RatingStarType[]
 }
 
 export const Rating = (props: RatingPropsType) => {
 
+    const mapStar = props.star.map(el => <Star selected={props.value >= el} setRating={props.setRatingValue} value={el}/>)
+
     return (
         <div>
-            <Star selected={props.value > 0} setRating={props.setRatingValue} value={1}/>
-            <Star selected={props.value > 1} setRating={props.setRatingValue} value={2}/>
-            <Star selected={props.value > 2} setRating={props.setRatingValue} value={3}/>
-            <Star selected={props.value > 3} setRating={props.setRatingValue} value={4}/>
-            <Star selected={props.value > 4} setRating={props.setRatingValue} value={5}/>
+            {mapStar}
         </div>
     );
 }
@@ -26,11 +25,11 @@ type StarPropsType = {
     value: RatingStarType
 }
 export const Star = (props: StarPropsType) => {
-    const finalStarStyle = (props.selected ? {backgroundColor: 'yellow'} : {backgroundColor: 'white'})
+    const finalStarStyle = props.selected ? {backgroundColor: 'yellow', display: 'inlineBlock'} : {backgroundColor: 'white'}
     const onClickStarHandler = () => {
         props.setRating(props.value)
     }
 
 
-    return <span className={s.star} style={finalStarStyle} onClick={onClickStarHandler}></span>
+    return <span className={s.star} style={finalStarStyle} onClick={onClickStarHandler}> </span>
 }
