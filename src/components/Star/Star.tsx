5648dc5e29ea7aca686abe67b9818467b9ac923b
Adammm <adammm.web@gmail.com>
2023-01-25 17:38:53 +0300

    refactor Star.tsx

--- src/components/Star/Star.tsx
+++ src/components/Star/Star.tsx
@@ -1,94 +1,32 @@
 import React, {useState} from "react";
+import s from './Star.module.css'
+
+export type RatingStarType = 0 | 1 | 2 | 3 | 4 | 5
 
 export const Rating = () => {
-    const [rating, setRating] = useState(0)
+
+    const [rating, setRating] = useState<RatingStarType>(0)
     return (
         <div>
-            <Star selected={rating > 0} setRating={()=>setRating(1)}/>
-            <Star selected={rating > 1} setRating={()=>setRating(2)}/>
-            <Star selected={rating > 2} setRating={()=>setRating(3)}/>
-            <Star selected={rating > 3} setRating={()=>setRating(4)}/>
-            <Star selected={rating > 4} setRating={()=>setRating(5)}/>
+            <Star selected={rating > 0} setRating={setRating} value={1}/>
+            <Star selected={rating > 1} setRating={setRating} value={2}/>
+            <Star selected={rating > 2} setRating={setRating} value={3}/>
+            <Star selected={rating > 3} setRating={setRating} value={4}/>
+            <Star selected={rating > 4} setRating={setRating} value={5}/>
         </div>
-        // if (props.value === 1) {
-        //     return (
-        //         <div>
-        //             <Star selected={true}/>
-        //             <Star selected={true}/>
-        //             <Star selected={true}/>
-        //             <Star selected={true}/>
-        //             <Star selected={true}/>
-        //         </div>
-        //     );
-        // }
-        // if (props.value === 2) {
-        //     return (
-        //         <div>
-        //             <Star selected={true}/>
-        //             <Star selected={true}/>
-        //             <Star selected={false}/>
-        //             <Star selected={false}/>
-        //             <Star selected={false}/>
-        //         </div>
-        //     );
-        // }
-        // if (props.value === 3) {
-        //     return (
-        //         <div>
-        //             <Star selected={true}/>
-        //             <Star selected={true}/>
-        //             <Star selected={true}/>
-        //             <Star selected={false}/>
-        //             <Star selected={false}/>
-        //         </div>
-        //     );
-        // }
-        // if (props.value === 4) {
-        //     return (
-        //         <div>
-        //             <Star selected={true}/>
-        //             <Star selected={true}/>
-        //             <Star selected={true}/>
-        //             <Star selected={true}/>
-        //             <Star selected={false}/>
-        //         </div>
-        //     );
-        // }
-        // if (props.value === 5) {
-        //     return (
-        //         <div>
-        //             <Star selected={true}/>
-        //             <Star selected={true}/>
-        //             <Star selected={true}/>
-        //             <Star selected={true}/>
-        //             <Star selected={true}/>
-        //         </div>
-        //     );
-        // }
-        // return (
-        //     <div>
-        //         <Star selected={false}/>
-        //         <Star selected={false}/>
-        //         <Star selected={false}/>
-        //         <Star selected={false}/>
-        //         <Star selected={false}/>
-        //     </div>
     );
 }
 type StarPropsType = {
     selected: boolean
-    setRating: () => void
+    setRating: (value: RatingStarType) => void
+    value: RatingStarType
 }
 export const Star = (props: StarPropsType) => {
-
-    let circle = {
-        width: '20px',
-        height: '20px',
-        border: '1px solid black',
-        display: 'inline-block',
-        margin: '5px',
-        backgroundColor: props.selected ? 'yellow' : 'white',
-        borderRadius: '10px'
+    const finalStarStyle = (props.selected ? {backgroundColor: 'yellow'} : {backgroundColor: 'white'})
+    const onClickStarHandler = () => {
+        props.setRating(props.value)
     }
-    return <span style={circle} onClick={props.setRating}></span>
+
+
+    return <span className={s.star} style={finalStarStyle} onClick={onClickStarHandler}></span>
 }
