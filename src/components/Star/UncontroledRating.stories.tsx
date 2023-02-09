81682674bdabe2b52fcf65b4d918818c06b37b3f
Adammm <adammm.web@gmail.com>
2023-02-09 17:26:08 +0300

    create stories UncontroledRating.stories.tsx

---
+++ src/components/Star/UncontroledRating.stories.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import {UncontrolledRating} from "./UncontroledStar";
+
+export default {
+    title: 'UncontrolledRating stories',
+    component: UncontrolledRating,
+}
+
+export const UncontrolledRatingChanged = () => {
+    return <UncontrolledRating/>
+}
