d2a5ee8f83bbea9ce231cba61b96ee7e6c20d4e8
Adammm <adammm.web@gmail.com>
2023-02-09 17:25:53 +0300

    create stories UncontroledOnOffButton.stories.tsx

---
+++ src/components/OnOffButton/UncontroledOnOffButton.stories.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import {UncontrolledOnOffButton} from "./UncontrolledOnOffButton";
+import '../../App.css'
+
+export default {
+    title: 'UncontrolledOnOffButton stories',
+    component: UncontrolledOnOffButton
+}
+
+export const UncontrolledOnOffButtonChanged = () => {
+    return <UncontrolledOnOffButton/>
+}
