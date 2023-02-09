c251b8ffe3995b199040ae3e263e47ce092dcadf
Adammm <adammm.web@gmail.com>
2023-02-09 17:28:55 +0300

    change component Star.tsx

--- src/components/Star/Star.tsx
+++ src/components/Star/Star.tsx
@@ -1,24 +1,7 @@
+import s from "../Rating/Rating.module.css";
 import React from "react";
-import s from './Star.module.css'
+import {RatingStarType} from "../Rating/Rating";
 
-export type RatingStarType = 1 | 2 | 3 | 4 | 5
-
-export type RatingPropsType = {
-    value: RatingStarType | 0
-    setRatingValue: (value:RatingStarType)=>void
-    star: RatingStarType[]
-}
-
-export const Rating = (props: RatingPropsType) => {
-
-    const mapStar = props.star.map(el => <Star selected={props.value >= el} setRating={props.setRatingValue} value={el}/>)
-
-    return (
-        <div>
-            {mapStar}
-        </div>
-    );
-}
 type StarPropsType = {
     selected: boolean
     setRating: (value: RatingStarType) => void
