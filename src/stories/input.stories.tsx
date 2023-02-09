031d8d799138287df7fbbc5472747de78a741a37
Adammm <adammm.web@gmail.com>
2023-02-09 18:51:51 +0300

    create logic for ControlledInput

--- src/stories/input.stories.tsx
+++ src/stories/input.stories.tsx
@@ -1,34 +1,58 @@
-import React, {useRef, useState} from 'react';
-import {OnOffButton} from "../components/OnOffButton/OnOffButton";
+import React, {ChangeEvent, KeyboardEvent, useRef, useState} from 'react';
 
 export default {
-  title: 'input',
-  component: OnOffButton,
+    title: 'input',
 };
 
 export const UncontrolledInput = () => <input type="text"/>;
 
 export const TrackValueOfUncontrolledInput = () => {
-  const [value, setValue] = useState('')
-  return <><input type="text" onChange={(e)=> {
-    const activeValue = e.currentTarget.value
-    setValue(activeValue)
-  }}/> ---- {value}</>
+    const [value, setValue] = useState('')
+    return <><input type="text" onChange={(e) => {
+        const activeValue = e.currentTarget.value
+        setValue(activeValue)
+    }}/> ---- {value}</>
 };
 
 export const GetValueOnButtonOfUncontrolledInput = () => {
-  const [value, setValue] = useState('')
-  const newRefForInput = useRef<HTMLInputElement>(null)
+    const [value, setValue] = useState('')
+    const newRefForInput = useRef<HTMLInputElement>(null)
 
-  const saveValue = () => {
-    if(newRefForInput.current) {
-      const newValue = newRefForInput.current.value
-      setValue(newValue)
-      newRefForInput.current.value = ''
+    const saveValue = () => {
+        if (newRefForInput.current) {
+            const newValue = newRefForInput.current.value
+            setValue(newValue)
+            newRefForInput.current.value = ''
+        }
     }
-  }
 
-  return <><input ref={newRefForInput} type="text"/><button onClick={saveValue}>save</button> nev value: {value}</>
+    return <><input ref={newRefForInput} type="text"/>
+        <button onClick={saveValue}>save</button>
+        nev value: {value}</>
+}
+
+export const ControlledInput = () => {
+    const [parentValue, setParentValue] = useState('')
+
+    const onChange = (e: ChangeEvent<HTMLInputElement>) => {
+        setParentValue(e.currentTarget.value)
+    }
+    const onBlur = () => {
+        setParentValue('')
+    }
+
+    const onKeyDown = (e: KeyboardEvent<HTMLInputElement>) => {
+        if (e.key === 'Enter') {
+            setParentValue('')
+        }
+    }
+
+    return <input
+        type="text"
+        value={parentValue}
+        onChange={onChange}
+        onBlur={onBlur}
+        onKeyDown={onKeyDown}/>
 }
 
 
