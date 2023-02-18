cb08f8d27585f4022f8f9f050b1d0d9f83f0a527
Adammm <adammm.web@gmail.com>
2023-02-18 18:19:23 +0300

    change reducer.ts

--- src/components/Accordion/reducer.ts
+++ src/components/Accordion/reducer.ts
@@ -1,11 +1,11 @@
 export type ActionType = {
     type: string
 }
-export type ReducerStateType = {
+export type StateType = {
     collapsed: boolean
 }
 export const TOGGLE_ACCORDION = "TOGGLE-ACCORDION"
-export const reducer = (state: ReducerStateType, action: ActionType) => {
+export const reducer = (state: StateType, action: ActionType) => {
     switch (action.type) {
         case TOGGLE_ACCORDION:
             return {
