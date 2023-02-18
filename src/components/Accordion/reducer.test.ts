c7f270855f37cb79f77fede1953d544427134307
Adammm <adammm.web@gmail.com>
2023-02-18 18:24:11 +0300

    change reducer.test.ts

--- src/components/Accordion/reducer.test.ts
+++ src/components/Accordion/reducer.test.ts
@@ -1,6 +1,6 @@
 import {reducer, StateType, TOGGLE_ACCORDION} from "./reducer";
 
-test('reducer should change value to opposite value', () => {
+test('reducer should change value to true', () => {
     let state: StateType = {
         collapsed: false
     }
@@ -9,4 +9,17 @@ test('reducer should change value to opposite value', () => {
 
     expect(state).not.toBe(changedState)
     expect(changedState.collapsed).toBe(!state.collapsed)
+    expect(changedState.collapsed).toBe(true)
+})
+
+test('reducer should change value to false', () => {
+    let state: StateType = {
+        collapsed: true
+    }
+
+    const changedState = reducer(state, {type: TOGGLE_ACCORDION})
+
+    expect(state).not.toBe(changedState)
+    expect(changedState.collapsed).toBe(!state.collapsed)
+    expect(changedState.collapsed).toBe(false)
 })
